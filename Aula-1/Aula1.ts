6a4e5bba80a8640898893963c45060a75ddc973c
Guilherme-Luis <guilherme_silva_luis@hotmail.com>
2021-06-25 20:54:52 -0300

    Aulas NLW

---
+++ Aula-1/Aula1.ts
@@ -0,0 +1,21 @@
+
+
+// nomeDaVariavel:TipoDaVariavel
+
+interface Usuario{
+    nome: string, email: string, telefone?: string
+}
+
+function enviarEmail({email, telefone, nome}: Usuario){
+
+    console.log(`Olá ${nome} seu email é ${email} e seu telefone é ${telefone}`);
+    //
+    //
+    //
+}
+
+enviarEmail({
+    email: "Dani.leao89@gmail.com",
+    nome: "Dani",
+    telefone: "1222121212121"
+});
